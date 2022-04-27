b42a4bf08210f1b9111de1b80a3d2e72403175f8
badr <badraithammou2000@gmail.com>
2022-04-27 14:59:47 +0000

    ezfffffffffff

---
+++ src/main/java/com/example/stage_back/StageBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.stage_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StageBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StageBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/stage_back/StageBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stage_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StageBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
