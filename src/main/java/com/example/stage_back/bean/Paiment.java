65d204c6341682a542e074cc10503c1c869e6c5a
khalil <aboulouanek@gmail.com>
2022-04-30 01:38:52 +0000

    add All package ( MS )

--- src/main/java/com/example/stage_back/bean/Paiment.java
+++ src/main/java/com/example/stage_back/bean/Paiment.java
@@ -1,6 +1,7 @@
 package com.example.stage_back.bean;
 
 
+import com.example.stage_back.enums.TypePaiment;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -22,6 +23,8 @@ public class Paiment {
     @Temporal(TemporalType.DATE)
     private Date datePaiement;
 
+    @Enumerated(EnumType.STRING)
+    private TypePaiment typePaiment;
 
 
 
